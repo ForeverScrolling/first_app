d40afeb597aa43c85a45ebc66e27a4349da9794c
ForeverScrolling <chris@foreverscrolling.com>
2013-10-14 19:06:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '160383a31221378d4995119e42fcb96b3f3344c27c7af6e2f8afb9d3e1e3101845db6aee26d7794eedc76ad71e4413f5b28255106f1ab30860287fa85a50df60'
